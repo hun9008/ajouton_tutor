79a2c640c002c20b3ff7716bbc9eccec56d7f1ef
tinon1004 <tinon1004@ajou.ac.kr>
2023-10-29 06:30:37 +0900

    Merge the code

--- front/ajouton/src/App.js
+++ front/ajouton/src/App.js
@@ -1,12 +1,5 @@
 
 import Login from './login';
-import Submit from './submit'; // Submit 컴포넌트를 임포트
-import './App.css';
-import './submit.css';
-
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
-import Random from './Random';
 import TutorApplication from './TutorApplication';
 import MentorReport from './MentorReport';
 import Header from './Header';
@@ -17,7 +10,6 @@ import LandgingPage from './LandingPage';
 function App() {
   return (
     <div className="App">
-
       <Router>
         <Header />
         <Routes>
@@ -32,7 +24,6 @@ function App() {
           <Route path='/submit' element={<Submit />} />
         </Routes>
       </Router>
-
     </div>
   );
 }
