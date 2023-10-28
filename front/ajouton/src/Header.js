07d3c5d4950c50f222afcaf968e31db68b708aad
tinon1004 <tinon1004@ajou.ac.kr>
2023-10-28 23:21:27 +0900

    Feat: header submenu

--- front/ajouton/src/Header.js
+++ front/ajouton/src/Header.js
@@ -1,7 +1,8 @@
 import React from "react";
+import { Link } from "react-router-dom"; 
+import MetorReport from "./MentorReport";
 
 function Header() {
-
     const headerStyle = {
         background: "#405678",
         display: "flex",
@@ -12,9 +13,26 @@ function Header() {
         flexShrink: 0,
     }
 
+    const subMenu = {
+        display: "flex",
+        flexShrink: 0,
+        marginLeft: "1000px",
+        gap : "15px",
+        color:"#FFF",
+        size: "18px",
+        style: "normal",
+        weight: "700",
+    }
+
     return (
         <div style={headerStyle}>
             <h1 className="title">멘토</h1>
+            <div style={subMenu}>
+                <p>예약 링크 생성</p>
+               
+                    <p Link to="/MentorReport">활동일지 내보내기</p>
+               
+            </div>
         </div>
     );
 }
